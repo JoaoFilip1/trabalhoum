3a9af58eb1c8464183aee21fbdf1badf72053b99
Joao Filipi <joaofilipi89@icloud.com>
2025-03-15 15:53:17 +0000

    Creating main logo Connecting Menu and Game classes

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-print('Loop Start')
-while True:
-    # Check For All Events "Verificar eventos"
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Fechar Janela
-            quit() #fim do pygame
+game = Game()
+game.run()
