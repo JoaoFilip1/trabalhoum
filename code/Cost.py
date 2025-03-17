590818d7edf46fccdd778e30238f89d0a4d65a92
Joao Filipi <joaofilipi89@icloud.com>
2025-03-17 11:39:08 +0000

    Creating asset folder inserting the menu: background, sound and fonts creating the const fila

---
+++ code/Cost.py
@@ -0,0 +1,11 @@
+COLOR_WHITE = (255,255,255)
+
+MENU_OPTION = ('NOVO JOGO 1P',
+               'NOVO JOGO 2P - COPERATIVO',
+               'NOVO JOGO 2P - COMPETITIVO',
+               'RECORDE',
+               'SAIR')
+
+
+WIN_WIDTH = 639
+WIN_HEIGHT = 414
--- code/Game.py
+++ code/Game.py
@@ -1,6 +1,6 @@
-
 import pygame
 
+from code.Cost import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
@@ -8,17 +8,16 @@ class Game:
 
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run (self, ):
+    def run(self, ):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-
             # Check For All Events "Verificar eventos"
-            #for event in pygame.event.get():
-               # if event.type == pygame.QUIT:
-                    #pygame.quit() #Fechar Janela
-                   # quit() #fim do pygame
+            # for event in pygame.event.get():
+            # if event.type == pygame.QUIT:
+            # pygame.quit() #Fechar Janela
+            # quit() #fim do pygame
